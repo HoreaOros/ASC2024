0f7fd5144a85ac9db122f2030799ef999ee1ff49
Horea Oros <horea.oros@gmail.com>
2024-10-07 09:58:48 +0300

    add conversion project

---
+++ 1007/1007/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _1007
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1007;
+
+            convertToBase(n, 16);
+        }
+
+        private static void convertToBase(int n, int b)
+        {
+            int cifra;
+            Stack<int> stack = new Stack<int>();
+            while(n > 0)
+            {
+                cifra = n % b;
+                stack.Push(cifra);
+                n = n / b;
+            }
+
+            while(stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+        }
+    }
+}
