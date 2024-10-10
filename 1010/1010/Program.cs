3f2ebe8c7685755c3330d68277c63f5ce65d6988
Horea Oros <horea.oros@gmail.com>
2024-10-10 09:51:49 +0300

    add 10.10 project

---
+++ 1010/1010/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+int n = 47;
+int b = 2;
+Console.WriteLine($"n = {n}(10) = {ConvertFromBase10(n, b)}({b})");
+
+
+
+///<summary>
+///Convertește numărul n din baza 10 in baza b
+///</summary>
+string ConvertFromBase10(int n, int b)
+{
+    if(b < 2 || b > 16)
+    {
+        throw new ArgumentException("Baza trebuie sa fie intre 2 si 16");
+    }
+
+    Stack<int> stack = new Stack<int>();
+    int rest;
+    while(n > 0)
+    {
+        rest = n % b;
+        stack.Push(rest);
+        n = n / b;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while(stack.Count > 0)
+    {
+        sb.Append(stack.Pop());
+    }
+    return sb.ToString();
+}
