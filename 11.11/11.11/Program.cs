11b100695538bbdd86768cfcbbb3765c3e0f7657
Horea Oros <horea.oros@gmail.com>
2024-11-11 09:54:09 +0200

    add AOC 2020 problem 1

---
+++ 11.11/11.11/Program.cs
@@ -0,0 +1,54 @@
+﻿string lines = File.ReadAllText("input.txt");
+
+string[] numbers = lines.Split(Environment.NewLine);
+
+int[] nums = new int[numbers.Length];
+
+for (int i = 0; i < nums.Length; i++)
+{
+    nums[i] = int.Parse(numbers[i]);
+}
+
+
+const int S = 2020;
+
+Part1(nums, S);
+Part2(nums, S);
+
+void Part2(int[] nums, int s)
+{
+    int result = 0;
+    bool found = false;
+    for (int i = 0; !found && i < nums.Length; i++)
+        for (int j = i + 1; !found && j < nums.Length; j++)
+            for (int k = j + 1; ! found && k < nums.Length; k++)
+            if (nums[i] + nums[j] + nums[k] == S)
+            {
+                result = nums[i] * nums[j] * nums[k];
+                found = true;
+            }
+
+
+    Console.WriteLine(result);
+}
+
+static void Part1(int[] nums, int S)
+{
+    int result = 0;
+    bool found = false;
+    for (int i = 0; !found && i < nums.Length; i++)
+        for (int j = i + 1; !found && j < nums.Length; j++)
+            if (nums[i] + nums[j] == S)
+            {
+                result = nums[i] * nums[j];
+                found = true;
+            }
+
+
+    Console.WriteLine(result);
+}
+
+/*
+ * 1 4 7 9 10 (pt ordonare O(n Log n)
+ * 
+ * */
